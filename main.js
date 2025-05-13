aec7ac56f3ed6f95f30cbc014388418d99d4be83
CARLOS-012 <160601956+CARLOS-012@users.noreply.github.com>
2025-05-13 09:09:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ for(let i=0;i <botoes.length;i++){
     botoes[i].onclick=function(){
         botoes[i].classList.add("ativo");}
        
-        for(let j=0; j <botoes.length; j++){
+        for(let j=o;j <botoes.length;j++){
             botoes[j].classList.remove("ativo")
-            botoes [i].classList.add("ativo")
+            
         }
     }
-
- 
